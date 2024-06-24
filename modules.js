45b5a4ebc679e465b9f5b139631124f749bf4f49
Archanaa <nkanniga04@gmail.com>
2024-06-24 13:54:53 +0530

    working on it

--- modules.js
+++ modules.js
@@ -1,3 +1,12 @@
 const xyz = require('./people')
 
-console.log(xyz)
+// console.log(xyz) // we will get it as objects 
+// //  {
+//     people: [ 'yoshi', 'yu', 'archu', 'viffy' ],
+//     ages: [ 25, 30, 20, 20 ]
+//   }
+
+console.log(xyz.people, xyz.ages)
+
+// [ 'yoshi', 'yu', 'archu', 'viffy' ]
+// [ 'yoshi', 'yu', 'archu', 'viffy' ] [ 25, 30, 20, 20 ]
--- people.js
+++ people.js
@@ -1,3 +1,12 @@
 const people = ['yoshi','yu','archu','viffy'];
+const ages = [25,30,20,20];
 
 console.log(people)
+// creating a property of the people here ,if people property name is same as people const 
+// doesnt want to explicitly mention it
+
+module.exports = {
+    people, ages
+};
+
+// the object xyz incorporates these 2 values people and ages 
