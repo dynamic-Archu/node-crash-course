3698e57f1ef9264d300c66526c4dc1d0c7bffc9d
Archanaa <nkanniga04@gmail.com>
2024-06-24 15:11:16 +0530

    modules update

--- modules.js
+++ modules.js
@@ -1,4 +1,9 @@
-const xyz = require('./people')
+// const xyz = require('./people')
+
+const { people , ages} = require('./people')
+
+// inorder to get many imports we use const { and add names here it need not to be the same name as in the other file 
+// but how much ever value is given here only gets imported }
 
 // console.log(xyz) // we will get it as objects 
 // //  {
@@ -6,7 +11,12 @@ const xyz = require('./people')
 //     ages: [ 25, 30, 20, 20 ]
 //   }
 
-console.log(xyz.people, xyz.ages)
+console.log(people, ages);
 
+const os = require('os');
+console.log(os);
+console.log(os.platform(), os.homedir());
+
+// console.log(xyz.people, xyz.ages)
 // [ 'yoshi', 'yu', 'archu', 'viffy' ]
 // [ 'yoshi', 'yu', 'archu', 'viffy' ] [ 25, 30, 20, 20 ]
